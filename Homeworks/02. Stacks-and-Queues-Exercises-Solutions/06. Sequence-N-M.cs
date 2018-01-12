7d23ed9c43457d3db8f84a1f1bf201fb75c508b8
Vania Vasileva <vani4ka66@abv.bg>
2018-01-12 15:25:35 +0200

    commit

---
+++ Homeworks/02. Stacks-and-Queues-Exercises-Solutions/06. Sequence-N-M.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Sequence_N_M
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            int[] arr = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int n = arr[0];
+            int m = arr[1];
+
+            queue.Enqueue(m);
+
+            if (m >= n)
+            {
+                
+                while (m/2 >= n)
+                {
+                    m /= 2;
+                    queue.Enqueue(m);
+                }
+
+                while (m - 2 >= n)
+                {
+                    m -= 2;
+                    queue.Enqueue(m);
+                }
+
+                while (m - 1 >= n)
+                {
+                    m -= 1;
+                    queue.Enqueue(m);
+                }
+
+                Console.WriteLine(string.Join(" -> ", queue.Reverse()));
+            }
+        }
+    }
+}
