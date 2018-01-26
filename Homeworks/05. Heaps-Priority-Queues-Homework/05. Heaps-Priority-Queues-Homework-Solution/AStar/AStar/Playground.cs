8133385b634df9c28909c84da7355f5a8d98398a
Vania Vasileva <vani4ka66@abv.bg>
2018-01-26 14:45:41 +0200

    commit

---
+++ Homeworks/05. Heaps-Priority-Queues-Homework/05. Heaps-Priority-Queues-Homework-Solution/AStar/AStar/Playground.cs
@@ -0,0 +1,104 @@
+﻿using System;
+
+public class Playground
+{
+    private static char[,] map =
+    {
+                    { '-', '-', '-', '-', '-' },
+                    { '-', '*', '-', '-', '-' },
+                    { 'W', 'W', 'W', 'W', 'W' },
+                    { '-', '-', '-', 'P', '-' },
+                    { '-', '-', '-', '-', '-' },
+         };
+
+    /*private static char[,] map =
+    {
+                    { '-', '-', '-', '-', '-', '-', '-', '-' },
+                    { '-', '-', '*', '-', '-', '-', '-', '-' },
+                    { 'W', 'W', 'W', 'W', 'W', '-', '-', '-' },
+                    { '-', '-', '-', '-', 'W', 'W', '-', '-' },
+                    { '-', '-', '-', 'P', 'W', '-', '-', '-' },
+                    { '-', '-', '-', '-', '-', '-', '-', '-' }
+         };*/
+
+    static void Main()
+    {
+        //FCost = GCost + HCost;
+
+        //FCost - distance from current  to the start;
+        //HCost - distance from current to the destination;
+        //Traverses the nodes with lowest f-cost first
+        //Updates the f-costs of their neighbors
+
+        //map = ReadMap();
+
+        var start = FindGoal('P');
+        var goal = FindGoal('*');
+
+        var aStar = new AStar(map);
+        var path = aStar.GetPath(start, goal);
+
+        foreach (var node in path)
+        {
+            var row = node.Row;
+            var col = node.Col;
+            map[row, col] = '@';
+        }
+
+        PrintMap();
+
+    }
+
+    private static char[,] ReadMap()
+    {
+        var n = int.Parse(Console.ReadLine());
+        var map = new char[n, n];
+        for (int i = 0; i < n; i++)
+        {
+            char[] line = Console.ReadLine().ToCharArray();
+
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                map[i, j] = line[j];
+            }
+        }
+
+        return map;
+    }
+
+    static Node FindGoal(char goal)
+    {
+        for (int row = 0; row < map.GetLength(0); row++)
+        {
+            for (int col = 0; col < map.GetLength(1); col++)
+            {
+                if (map[row, col] == goal)
+                {
+                    return new Node(row, col);
+                }
+            }
+        }
+
+        throw new ArgumentException("Object not present on map");
+    }
+
+    static void PrintMap()
+    {
+        for (int row = 0; row < map.GetLength(0); row++)
+        {
+            for (int col = 0; col < map.GetLength(1); col++)
+            {
+                if (map[row, col] == '@')
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+
+                Console.Write(map[row, col]);
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Homeworks/07.1. AVL-Trees-AA-Trees-Lab/07. AVL-Trees-AA-Trees-CSharp-Lab-Solution/AVLTree/AVLTree/AVL.cs
@@ -0,0 +1,157 @@
+﻿using System;
+
+public class AVL<T> where T : IComparable<T>
+{
+    public AVL()
+    {
+        
+    }
+
+    private Node<T> root;
+
+    public Node<T> Root
+    {
+        get
+        {
+            return this.root;
+        }
+    }
+
+    public bool Contains(T item)
+    {
+        var node = this.Search(this.root, item);
+        return node != null;
+    }
+
+    public void Insert(T item)
+    {
+        this.root = this.Insert(this.root, item);
+    }
+
+    private Node<T> Insert(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return new Node<T>(item);
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            node.Left = this.Insert(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            node.Right = this.Insert(node.Right, item);
+        }
+
+        node = Balance(node);
+        UpdateHeight(node);
+
+        return node;
+    }
+
+    private Node<T> Search(Node<T> node, T item)
+    {
+        if (node == null)
+        {
+            return null;
+        }
+
+        int cmp = item.CompareTo(node.Value);
+        if (cmp < 0)
+        {
+            return Search(node.Left, item);
+        }
+        else if (cmp > 0)
+        {
+            return Search(node.Right, item);
+        }
+
+        return node;
+    }
+
+    public void EachInOrder(Action<T> action)
+    {
+        this.EachInOrder(this.root, action);
+    }
+
+    private void EachInOrder(Node<T> node, Action<T> action)
+    {
+        if (node == null)
+        {
+            return;
+        }
+
+        this.EachInOrder(node.Left, action);
+        action(node.Value);
+        this.EachInOrder(node.Right, action);
+    }
+
+    private static int Height(Node<T> node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+
+        return node.Height;
+    }
+
+    private static void UpdateHeight(Node<T> node)
+    {
+        node.Height = Math.Max(Height(node.Left), Height(node.Right)) + 1;
+    }
+
+    private static Node<T> RotateRight(Node<T> node )
+    {
+        var newRoot = node.Left;
+        node.Left = newRoot.Right;
+        newRoot.Right = node;
+
+        UpdateHeight(node);
+
+        return newRoot;
+    }
+
+    private static Node<T> RotateLeft(Node<T> node)
+    {
+        var newRoot = node.Right;
+        node.Right = newRoot.Left;
+        newRoot.Left = node;
+
+        UpdateHeight(node);
+
+        return newRoot;
+    }
+
+    private static Node<T> Balance(Node<T> node)
+    {
+        int balance = Height(node.Left) - Height(node.Right);
+
+        if (balance < -1)
+        {
+            int childBalance = Height(node.Right.Left) - Height(node.Right.Right);
+            if (childBalance > 0)
+            {
+                node.Right = RotateRight(node.Right);
+            }
+
+            return RotateLeft(node);
+        }
+        else if (balance > 1)
+        {
+            int childBalance = Height(node.Left.Left) - Height(node.Left.Right);
+            if (childBalance < 0)
+            {
+                node.Left = RotateLeft(node.Left);
+            }
+
+            return RotateRight(node);
+        }
+
+        return node;
+    }
+
+
+}
